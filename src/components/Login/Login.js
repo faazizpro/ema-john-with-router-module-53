e8d83000f70dd79f9f704324798da88873a32f28
faazizpro <fahadafs11@gmail.com>
2022-10-22 10:02:06 +0600

    Login Form Created Done

---
+++ src/components/Login/Login.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Login.css';
+
+const Login = () => {
+    return (
+        <div className='form-container'>
+            <h1 className='form-title'>Login</h1>
+            <form>
+                <div className="form-control">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" required />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" required />
+                </div>
+                <input className='btn-submit' type="submit" value="Login" />
+            </form>
+            <h4 className='link-signup'>New to Ema-john? <span className='link-color'><Link to='/signup'>Create New Account</Link></span></h4>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAuYz0Do55445bInabzjC--vg6FXlkAuMY",
+  authDomain: "ema-john-simple-38d26.firebaseapp.com",
+  projectId: "ema-john-simple-38d26",
+  storageBucket: "ema-john-simple-38d26.appspot.com",
+  messagingSenderId: "540651280061",
+  appId: "1:540651280061:web:ddc2340f7c3d47727c49d4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
